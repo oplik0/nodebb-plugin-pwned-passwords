2813dd142278c4170cc17df3f2f9b3d417dbb5da
Ben Lubar <ben.lubar@gmail.com>
2018-08-06 06:09:49 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Checks passwords against Troy Hunt's Have I Been Pwned Pwned Passwords API.
+
+![](https://packages.nodebb.org/api/v1/plugins/nodebb-plugin-pwned-passwords/compatibility.png)
--- library.js
+++ library.js
@@ -2,18 +2,27 @@
 
 const crypto = require('crypto');
 const https = require('https');
+const LRU = require('lru-cache');
 const User = module.parent.require('./user');
+
+const cache = LRU(100);
 
 const originalIsPasswordValid = User.isPasswordValid;
 
 User.isPasswordValid = function (password, minStrength, callback) {
 	const cb = typeof minStrength === 'function' && !callback ?
 		minStrength : callback;
+	const args = Array.prototype.slice.call(arguments, 0);
 
 	const hash = crypto.createHash('sha1');
 	hash.update(password);
-
 	const sha1 = hash.digest('hex').toUpperCase();
+
+	const cachedHashes = cache.get(sha1.substring(0, 5));
+	if (cachedHashes) {
+		return checkPassword(sha1, cachedHashes, args, cb);
+	}
+
 	https.get('https://api.pwnedpasswords.com/range/' + sha1.substring(0, 5), function (res) {
 		var body = '';
 		res.on('data', function (data) {
@@ -21,16 +30,21 @@ User.isPasswordValid = function (password, minStrength, callback) {
 		});
 		res.on('end', function () {
 			const hashes = body.split(/\r?\n/g);
-			const bad = hashes.find(function (line) {
-				return line.substring(0, 36) === sha1.substring(5) + ':';
-			});
-			if (bad) {
-				cb(new Error('[[pwned-passwords:bad-password, ' + parseInt(bad.substring(36), 10) + ']]'));
-				return;
-			}
-			originalIsPasswordValid.apply(User, arguments);
+			cache.set(sha1.substring(0, 5), hashes);
+			checkPassword(sha1, hashes, args, cb);
 		});
 	}).on('error', function (err) {
 		cb(err);
 	});
 };
+
+function checkPassword(sha1, hashes, args, callback) {
+	const bad = hashes.find(function (line) {
+		return line.substring(0, 36) === sha1.substring(5) + ':';
+	});
+	if (bad) {
+		callback(new Error('[[pwned-passwords:bad-password, ' + parseInt(bad.substring(36), 10) + ']]'));
+		return;
+	}
+	originalIsPasswordValid.apply(User, args);
+}
